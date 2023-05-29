207d88c11234b4aa1b21d8e3b223143881e1dceb
Jramongs18 <jramongs15@gmail.com>
2023-05-29 02:57:14 -0600

    Cambios al App

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,38 @@ import { Contador } from './components/Contador';
 import { ContadorConHook } from "./components/ContadorConHook";
 import { Login } from "./components/Login";
 import { Usuarios } from "./components/Usuarios";
-
 import { Formularios } from "./components/Formularios";
 
 const App = () => {
   return (
     <div className='mt-2'>
-      <h1>Introduccion a TS -React</h1>
+      <h1>Introducción a TS - React</h1>
       <hr />
+      <br />
       <TiposBasicos />
+      <br />
+      <hr />
       <ObjetosLiterales />
+      <br />
+      <hr />
       <Funciones />
+      <br />
+      <hr />
       <Contador />
+      <br />
+      <hr />
       <ContadorConHook />
+      <br />
+      <hr />
       <Login />
+      <br />
+      <hr />
       <Usuarios />
+      <br />
+      <hr />
       <Formularios />
+      <br />
+      <hr />
 
     </div>
   )
