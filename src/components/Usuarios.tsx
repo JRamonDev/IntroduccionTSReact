f6b01a07739eba8e98cbee293948e0b4db5b5ea8
Jramongs18 <jramongs15@gmail.com>
2023-05-29 02:46:21 -0600

    Agregando usuarios

---
+++ src/components/Usuarios.tsx
@@ -0,0 +1,38 @@
+import { useUsuarios } from "./hooks/useUsuarios";
+
+
+export const Usuarios = () => {
+
+    const { usuarios, paginaAnterior, paginaSiguiente, renderItem } = useUsuarios();
+
+    return (
+        <>
+            <h3>Usuarios:</h3>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usuarios.map(renderItem)}
+                </tbody>
+            </table>
+            <button
+                className="btn btn-danger"
+                onClick={paginaAnterior}
+            >
+                Anteriores
+            </button>
+            &nbsp;
+            <button
+                className="btn btn-primary"
+                onClick={paginaSiguiente}
+            >
+                Siguientes
+            </button>
+        </>
+    )
+}
